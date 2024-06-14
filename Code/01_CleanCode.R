5965b912d78c44760037706bc329cd5870be51af
Alessia Kettlitz <aekettlitz@gmail.com>
2024-06-14 14:05:48 -0400

    final details to index by continent over time plot, exporting to png

--- Code/01_CleanCode.R
+++ Code/01_CleanCode.R
@@ -30,32 +30,40 @@ data_countries$Continent <- countrycode(sourcevar = data_countries$Entity, origi
   # countries or continents over time
     # can set that the countries are shown for respective regions or continents selected!
 
-theme_set(theme_minimal(base_family = "Lato"))
+theme_set(theme_minimal(base_family = "Lato", base_size = 18))
 
-data_clean %>%
-  filter(Entity == "Africa" | Entity == "Asia" | Entity == "Europe"
-         | Entity == "North America" | Entity == "Oceania" | Entity == "South America") %>% 
-  ggplot(aes(Year, PolicyIndex, color=Entity)) +
+# line plot, index by continent over time
+plot <- data_clean %>%
+  filter(Entity == "Africa" | Entity == "Asia" | Entity == "Europe" |
+           Entity == "North America" | Entity == "Oceania" | Entity == "South America") %>%
+  ggplot(aes(Year, PolicyIndex, color = Entity)) +
   geom_line(size = 1) +
-  labs(x = "Year", y = "LGBT+ Rights Index", 
-       title = "Unequal Celebrations: Disparities in LGBT+ Rights Across the Globe (1991-2019)",
-       subtitle = "This line graph showcases the disparity in equality for LGBT+ individuals across the globe. It highlights the diverse trajectories regions of the world have been on to improve the rights for LGBT+ people - illuminating 
-the diverse lived experiences of LGBT+ communities globally. The index measures a country's policy equality between LGBT+ and non-LGBT+ residents. Key highlights include the significant surge observed 
-in South America over the past decade reflecting the impact of cultural movements in this region. Conversely, the stagnant change in Africa underscores the ongoing challenges in this region for LGBT+ people.",
-       caption = "Data Source: Kristopher Velasco (2020) – processed by Our World in Data") +
-  theme(legend.position = "top",
-        legend.title = element_blank(),
-        axis.title.x = element_text(size = 15),
-        axis.title.y = element_text(size = 20),
-        legend.text = element_text(size = 12),
-        plot.title = element_text(size = 35),
-        plot.subtitle = element_text(size = 15),
-        plot.caption = element_text(hjust = 0, size = 10)) +
-  guides(
-    color = guide_legend(nrow = 1, byrow = TRUE, override.aes = list(size = 5))
-  )
+  labs(
+    x = "Year", 
+    y = "LGBT+ Rights Index",
+    title = "Unequal Celebrations: Disparities in LGBT+ Rights Across the Globe (1991-2019)",
+    subtitle = "This line graph showcases the disparity in equality for LGBT+ individuals across the globe. It highlights the varying trajectories regions of the world have been on to improve the rights for LGBT+ people - illuminating the 
+diverse lived experiences of LGBT+ communities globally. The index measures a country's policy equality between LGBT+ and non-LGBT+ residents. Key highlights include the significant surge observed in South America
+over the past decade reflecting the impact of cultural movements in this region. Conversely, the stagnant change in Africa underscores the ongoing challenges in this region for LGBT+ people.",
+    caption = "Data Source: Kristopher Velasco (2020) – processed by Our World in Data"
+  ) +
+  theme(
+    legend.position = "right",
+    legend.title = element_blank(),
+    legend.text = element_text(size = 30),
+    axis.title.x = element_text(size = 30),
+    axis.title.y = element_text(size = 30),
+    axis.text = element_text(size = 25),
+    plot.title = element_text(size = 73, face = "bold"),
+    plot.subtitle = element_text(size = 28, lineheight = .5),
+    plot.caption = element_text(hjust = 0, size = 25))
 
-
-
-
-
+ggsave(
+  filename = "lgbtrights_continent.png",
+  plot = plot,
+  width = 12.5,   
+  height = 6.54,  
+  units = "in",   
+  dpi = 300,      
+  bg = "white"    
+)
